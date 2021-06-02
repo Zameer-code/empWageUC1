a4698c160119d952b091bac06a64b5ac17d29672
Zameer-code <zameerraone96@gmail.com>
2021-06-02 18:58:37 +0530

    EmployeeWageUC1.java

---
+++ EmployeeWageUC1.java
@@ -0,0 +1,20 @@
+package com.employeewage;
+
+public class EmployeeWageUC1 {
+
+	public static void main(String[] args) {
+		// Constants
+		int IS_FULL_TIME = 1;
+		// Computation
+		double empCheck = Math.floor(Math.random() *10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
+
+	}
+}
+
+
+
